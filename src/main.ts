8969a35801672e41bbe7810ab5f0ef43c32e8daf
Sacramentix <benji.u1225u@gmail.com>
2022-04-21 10:54:43 +0200

    working pwa ?

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onOfflineReady() {},
+})
 
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,37 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA()
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        sourcemap: true,
+        cleanupOutdatedCaches: false  
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+      manifest: {
+        name: 'Frechi',
+        short_name: 'Frechi',
+        description: 'Frechi le jeu des chiffre',
+        theme_color: '#222222',
+        icons: [
+          {
+            src: 'android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: "image/svg+xml",
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })
   ]
 })
